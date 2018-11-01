e3aacec036f83db6c746859c58884610cedcdd81
Aleksandar Bosancic <aleksandar.bosancic@devlabs.ba>
2018-11-01 08:39:10 +0100

    Init commit added sprign boot structure

---
+++ src/main/java/com/abosancic/reload/ReloadApplication.java
@@ -0,0 +1,12 @@
+package com.abosancic.reload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReloadApplication.class, args);
+	}
+}
